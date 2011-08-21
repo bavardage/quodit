dd810be2eb0f994733c4ae13f26fa786176269d3
Dmitriy Brezhnev <brezhnd@gmail.com>
2011-08-20 20:17:05 -0700

    submitting quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,4 +5,19 @@ class QuotesController < ApplicationController
     @quotes = @wall.quotes
   end
   
+  def destroy
+   
+    @quote = Quote.find(params[:id])
+    if (@quote.author == current_user)
+      @quote.destroy
+    end
+    respond_to do |format|
+      format.html { redirect_to(Wall.find(params[:wall_id]) }
+    end
+  end 
+
+  def new
+    @wall = Wall.find(params[:wall_id])
+    @quote = Quote.new
+  end
 end
