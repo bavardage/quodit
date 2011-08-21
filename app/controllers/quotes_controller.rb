1c8f21c11feed320c97dff2b63d6d48175cfc80e
Ben Duffield <jebavarde@gmail.com>
2011-08-20 22:26:50 -0700

    quotes#create

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -20,4 +20,24 @@ class QuotesController < ApplicationController
     @wall = Wall.find(params[:wall_id])
     @quote = Quote.new
   end
+
+  def create
+    @wall = Wall.find(params[:wall_id])
+    @quote = Quote.new params[:quote]
+    
+    @quote.author = current_user
+    @quote.wall = @wall
+
+    #TODO: should also here extract tagged users!
+
+    respond_to do |format|
+      if @quote.save
+        format.html { redirect_to wall_path(@wall) }
+        format.json { render :json => quote }
+      else
+        format.html { render :action => :new }
+        format.json { @obj = wall; render "layouts/errors.json" }
+      end
+    end
+  end
 end
