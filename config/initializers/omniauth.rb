dec14d6a510e15c8437ce2674958682b5c354015
unknown <Que@.(none)>
2011-08-21 14:42:50 -0700

    Tweaks to work on windows

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   FB_APP_SECRET = '562d30b89c49f380bd7565f258ca3a92'
   provider :facebook, FB_APP_ID, FB_APP_SECRET, {
     :scope => 'publish_stream,offline_access,email',
-    :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+    :client_options => {:ssl => {:ca_path => "/etc/ssl/certs", :ca_file => "#{Rails.root}/config/ca-bundle.crt"}}}
   
 end
