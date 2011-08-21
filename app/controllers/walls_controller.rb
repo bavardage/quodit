e3d8eb06bbc267069d87e37db2ff7fab85161f15
Ben Duffield <jebavarde@gmail.com>
2011-08-20 20:16:45 -0700

    make some new views, controller actions

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -4,6 +4,10 @@ class WallsController < ApplicationController
     @wall = Wall.new
   end
 
+  def show
+    @wall = Wall.find(params[:id])
+  end
+
   def create
     @wall = Wall.new params[:wall]
     @wall.users << current_user
@@ -16,6 +20,30 @@ class WallsController < ApplicationController
         format.json { render :json => @wall }
       else
         format.html { render :action => "new", flash[:notice] => "Could not create wall" }
+        format.json do
+          @obj = wall
+          render "layouts/errors.json"
+        end
+      end
+    end
+  end
+
+  def edit
+    @wall = Wall.find(params[:id])
+  end
+
+  def update
+    @wall = Wall.find(params[:id])
+    @wall.update_attributes(params[:wall])
+    
+    respond_to do |format|
+      if @wall.save
+        format.html do
+          redirect_to wall_path(@wall)
+        end
+        format.json { render :json => true }
+      else
+        format.html { render :action => "edit", flash[:notice] => "Could not update wall" }
         format.json do
           @obj = wall
           render "layouts/errors.json"
