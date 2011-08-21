03794aa46f1072b3e0e50b3705e4c43b215eb57c
Ben Duffield <jebavarde@gmail.com>
2011-08-21 16:52:44 -0700

    add request to join, invite from facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.create_with_omniauth(auth)
     session[:user_id] = user.id
     session[:token] = auth["credentials"]["token"]
     redirect_to user_path(user), :notice => "Signed in!"
