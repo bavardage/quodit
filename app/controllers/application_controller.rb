926de55d602b2cd01fd18050b0742adc5bdfb5cf
Ben Duffield <jebavarde@gmail.com>
2011-08-21 19:02:55 -0700

    add redirect if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,19 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  before_filter :require_signed_in
+
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_signed_in
+    unless current_user
+      puts "NOT SIGNED IN"
+      redirect_to root_path, :notice => "You must be signed in!"
+    end
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 
+  skip_before_filter :require_signed_in, :only => [:create]
+
   def create
     auth = request.env["omniauth.auth"]
     user = User.create_with_omniauth(auth)
@@ -9,6 +11,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    puts "DESTROYING SESSION"
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
   end
